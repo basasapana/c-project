2d9c45e6c30226055c6db67be3686095a677076e
basasapana <basudev.bista@atlantisuniversity.edu>
2017-10-09 09:28:40 -0700

    The fizzbzz, array , primenumber

---
+++ fizz/fizz/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace fizz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 100; i++) {
+                string result = "";
+                if (i % 3 == 0) result = "fizz";
+                if (i % 5 == 0) result = result + "buzz";
+
+                Console.WriteLine(result);
+                if (result.Length == 0)
+                    Console.WriteLine(i);
+            }
+        }
+    }
+}
